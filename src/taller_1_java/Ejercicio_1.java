0f9b515ce643201a50e4878ca2bded66c26158d5
zullytamayom <zullytamayom@gmail.com>
2026-04-22 10:08:04 -0500

    ejercicio_1

--- src/taller_1_java/Ejercicio_1.java
+++ src/taller_1_java/Ejercicio_1.java
@@ -2,6 +2,15 @@ package taller_1_java;
 
 public class Ejercicio_1 {
     public static void main(String[] args) {
+        String productName = "Boton de Oro";
+        Double unitPrice = 35.500;
+        int stock = 120;
+        boolean available = true;
+        byte productCode = 1;
+
+        String result = String.format("El cafe es %s  y cuesta %f , tenemos en inventario %d , su disponibilidad es %b y el codigo de producto es %d", productName, unitPrice,stock,available,productCode);
+        System.out.println("result = " + result);
+
 
     }
 }
